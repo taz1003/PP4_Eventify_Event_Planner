24be9b9e680a225fa83f0cd4d14a6328f87a84ca
taz1003 <li9vyrit@students.codeinstitute.net>
2025-05-12 17:08:36 +0200

    Create secret key

--- eventify/settings.py
+++ eventify/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c6&$r218w@=zyb&4zu^!u_dema2x$7g7g32!a9no)ph8an&&+x'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '.127.0.0.1', 'localhost',]
 
