852e240b32dce6c3dff05e734f09cb40e0e39d72
taz1003 <li9vyrit@students.codeinstitute.net>
2025-05-12 18:03:52 +0200

    Create basic Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200, blank=100)
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="created_events")
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['date']
