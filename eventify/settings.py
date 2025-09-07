715ca3a55d99a0c56b13d67f443b1d6e0ab32d56
taz1003 <li9vyrit@students.codeinstitute.net>
2025-09-07 18:43:11 +0200

    Hide password reset link from login.html as some accounts might not have emails (only usernames)

--- eventify/settings.py
+++ eventify/settings.py
@@ -85,7 +85,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
 
